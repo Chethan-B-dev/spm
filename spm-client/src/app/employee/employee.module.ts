ef40550649f5047207a88f4d5bf9b906e1a6de42
Savya Sachi V B <savyanio@gmail.com>
2022-05-21 11:35:52 +0530

    Created the layout for employee dashboard

--- spm-client/src/app/employee/employee.module.ts
+++ spm-client/src/app/employee/employee.module.ts
@@ -4,21 +4,22 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/material/material.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeProjectDetailComponent } from './employee-project-detail/employee-project-detail.component';
 import { EmployeeTaskDetailComponent } from './employee-task-detail/employee-task-detail.component';
 import { EmployeeIssueDetailComponent } from './employee-issue-detail/employee-issue-detail.component';
-
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
-  declarations: [DashboardComponent, EmployeeProjectDetailComponent, EmployeeTaskDetailComponent, EmployeeIssueDetailComponent],
+  declarations: [ EmployeeProjectDetailComponent, EmployeeTaskDetailComponent, EmployeeIssueDetailComponent, EmployeeDashboardComponent],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
     ReactiveFormsModule,
     MaterialModule,
+    MatGridListModule
   ]
 })
 export class EmployeeModule { }
