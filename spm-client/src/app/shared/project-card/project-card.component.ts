b9480085789d31694a4bf39ebae869fc0a93d2a8
Chethan B <chethanborigin@gmail.com>
2022-05-30 20:32:57 +0530

    FE: test commit2

--- spm-client/src/app/shared/project-card/project-card.component.ts
+++ spm-client/src/app/shared/project-card/project-card.component.ts
@@ -42,7 +42,7 @@ export class ProjectCardComponent implements OnInit, OnDestroy {
   employees: IAppUser[] = [];
   private readonly destroy$ = new Subject();
   @Output() showIssues: EventEmitter<void> = new EventEmitter<void>();
-  changed;
+  changedagain;
 
   constructor(
     public dialog: MatDialog,
